84d4dcc57b65a3e568986a028b2f4b44e55c1c61
nazimjrf <nazim@jrfintel.com>
2025-01-26 15:27:21 +0600

    drag-drop-react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { DragAndDrop } from "./components/DragAndDrop"
+import { Title } from "./components/Title"
+
+const App = () => {
+
+  return (
+    <div className="container-main flex">
+      <Title />
+      <DragAndDrop />
+    </div>
+  )
+}
+export default App
